4f75bd101f86b2de94a695d27330bf6cec2cd4ea
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2024-06-26 17:39:13 +0700

    build: set build vendorHash

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     typescript-language-server.url = "github:NixOS/nixpkgs/9693852a2070b398ee123a329e68f0dab5526681";
   };
 
-  outputs = inputs@{ flake-parts, templ, go, gopls, ... }:
+  outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
@@ -21,10 +21,7 @@
           version = "2.0.0";
           subPackages = [ "cmd/wgserver" ];
           src = ./server;
-          vendorHash = "sha256-1wXTcB8QfeqWS9DUJtxPykSnMy2YeTRpkoZ10WJT6ig=";
-          preBuild = ''
-            cp -r ${self'.packages.wgserver-npm}/dist ./dist
-          '';
+          vendorHash = null;
         };
         packages.wgserver-docker = pkgs.dockerTools.buildImage {
           name = "muhrizqiardi/wgserver";
@@ -55,7 +52,7 @@
             inputs'.gnumake42.legacyPackages.gnumake42
             inputs'.go.legacyPackages.go
             inputs'.gopls.legacyPackages.gopls
-            inputs'.typescript-language-server.lecacyPackages.typescript-language-server
+            inputs'.typescript-language-server.legacyPackages.nodePackages.typescript-language-server
           ];
         };
       };
