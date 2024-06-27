165dfcc8aa42c604162a20e5cdf43235d6d3dcf5
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2024-06-27 09:06:38 +0700

    feat: add config

---
+++ server/internal/games/page/feature/homepage/handler_test.go
@@ -0,0 +1,47 @@
+package homepage
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/muhrizqiardi/wikipediagolf_v2/server/testutil"
+)
+
+func TestEndpoint(t *testing.T) {
+	t.Run("should not return 404", func(t *testing.T) {
+		var (
+			req      = httptest.NewRequest(http.MethodGet, "/", nil)
+			res      = httptest.NewRecorder()
+			tmpl     = MustNewTemplate(NewTemplate())
+			serveMux = http.NewServeMux()
+		)
+
+		Endpoint(tmpl)(serveMux)
+		serveMux.ServeHTTP(res, req)
+		testutil.AssertInequal(t, 404, res.Result().StatusCode)
+	})
+}
+
+func TestHandler(t *testing.T) {
+	t.Run("should return status code 200", func(t *testing.T) {
+		var (
+			req = httptest.NewRequest(http.MethodGet, "/", nil)
+			res = httptest.NewRecorder()
+		)
+		tmpl, err := NewTemplate()
+		if err != nil {
+			t.Error("exp nil; got err:", err)
+		}
+
+		Handler(tmpl).ServeHTTP(res, req)
+
+		var (
+			exp = 200
+			got = res.Result().StatusCode
+		)
+		if exp != got {
+			t.Errorf("exp %d; got %d", exp, got)
+		}
+	})
+}
---
+++ server/internal/games/shared/config/config_test.go
@@ -0,0 +1,61 @@
+package config
+
+import (
+	"testing"
+
+	"github.com/muhrizqiardi/wikipediagolf_v2/server/testutil"
+)
+
+func TestGetConfig(t *testing.T) {
+	type testCase struct {
+		args   []string
+		getenv func(string) string
+		exp    Config
+	}
+
+	tests := []testCase{
+		{
+			args: []string{"-host=localhost", "-port=3001", "-migrate", "-seed"},
+			getenv: func(s string) string {
+				switch s {
+				case "DATABASE_URL":
+					return "postgres://user:mockpw@localhost:5432/dbname"
+				}
+				return ""
+			},
+			exp: Config{
+				Host:        "localhost",
+				Port:        3001,
+				IsMigrate:   true,
+				IsSeed:      true,
+				DatabaseURL: "postgres://user:mockpw@localhost:5432/dbname",
+			},
+		},
+		{
+			args: []string{},
+			getenv: func(s string) string {
+				switch s {
+				case "DATABASE_URL":
+					return "postgres://user:mockpw@localhost:5432/dbname"
+				}
+				return ""
+			},
+			exp: Config{
+				Host:        "0.0.0.0",
+				Port:        3000,
+				IsMigrate:   false,
+				IsSeed:      false,
+				DatabaseURL: "postgres://user:mockpw@localhost:5432/dbname",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		var (
+			exp = test.exp
+			got = GetConfig(test.args, test.getenv)
+		)
+
+		testutil.AssertEqualCMP(t, exp, got)
+	}
+}
