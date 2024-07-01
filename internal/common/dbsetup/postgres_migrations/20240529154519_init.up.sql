eba2a279934eb7f42c4949527fc1c3341ca0109a
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2024-07-01 14:44:27 +0700

    feat: make `uid` field primary key
    Refs: #51

--- internal/common/dbsetup/postgres_migrations/20240529154519_init.up.sql
+++ internal/common/dbsetup/postgres_migrations/20240529154519_init.up.sql
@@ -1,5 +1,5 @@
 create table if not exists usernames (
-    uid text not null,
+    uid text not null primary key,
     username text not null unique,
     created_at timestamp default current_timestamp not null,
     updated_at timestamp default current_timestamp not null
