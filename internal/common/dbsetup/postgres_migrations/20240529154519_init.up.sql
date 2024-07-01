1978827c0bccece83972d898889956986c84d904
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2024-07-01 13:10:18 +0700

    fix: fix wrong table name
    Refs: #51

--- internal/common/dbsetup/postgres_migrations/20240529154519_init.up.sql
+++ internal/common/dbsetup/postgres_migrations/20240529154519_init.up.sql
@@ -1,4 +1,4 @@
-create table if not exists users (
+create table if not exists usernames (
     uid text not null,
     username text not null unique,
     created_at timestamp default current_timestamp not null,
