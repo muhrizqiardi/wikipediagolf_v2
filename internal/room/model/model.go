e15d603cd1b5de211ff0f3ea1a23da9932ffe4ee
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2024-07-04 09:00:48 +0700

    feat: add model and query
    Refs: #90

---
+++ internal/room/model/model.go
@@ -0,0 +1,25 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type RoomMember struct {
+	ID        uuid.UUID `db:"id"`
+	IsOwner   bool      `db:"is_owner"`
+	RoomID    int       `db:"room_id"`
+	UserUID   string    `db:"user_uid"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+}
+
+type Room struct {
+	ID          uuid.UUID `db:"id"`
+	Code        string    `db:"code"`
+	State       string    `db:"state"`
+	RoomMembers []RoomMember
+	CreatedAt   time.Time `db:"created_at"`
+	UpdatedAt   time.Time `db:"updated_at"`
+}
---
+++ internal/room/query/query.go
@@ -0,0 +1,63 @@
+package query
+
+const (
+	queryInsertRoom = `
+		insert into rooms (code, state) values ($1, $2)
+			returning id, code, state, created_at, updated_at;
+	`
+	queryInsertRoomMember = `
+		insert into room_members (room_id, user_uid, is_owner) values ($1, $2, $3)
+			returning id, is_owner, room_id, user_uid, created_at, updated_at;
+	`
+	queryDeleteRoomMember = `
+		delete from room_members
+			where room_id = $1 and user_uid = $2;
+	`
+	queryGetRoomByCode = `
+		select id, code, state, created_at, updated_at
+			from rooms
+			where code = $1;
+	`
+	queryGetRoomByID = `
+		select id, code, state, created_at, updated_at
+			from rooms
+			where id = $1;
+	`
+	queryGetRoomMembers = `
+		select 
+				rm.id as id, 
+				rm.owner_id as owner_id,
+				rm.room_id as room_id,
+				rm.user_uid as user_uid,
+				rm.created_at as created_at, 
+				rm.updated_at as updated_at
+			from rooms as r
+			inner join room_members as rm
+				on rm.room_id = r.id
+			where id = $1;
+	`
+	queryGetRoomBelongToMember = `
+		select 
+				r.id as id, 
+				r.code as code, 
+				r.state as state, 
+				r.created_at as created_at, 
+				r.updated_at as updated_at
+			from rooms as r
+			inner join room_members as rm
+				on rm.room_id = r.id
+			where rm.user_uid = $1;
+	`
+	queryUpdateRoomStatus = `
+		update rooms
+			set 
+				status = $2,
+				updated_at = current_timestamp
+			where id = $1
+			returning id, code, state, created_at, updated_at;
+	`
+	queryDelete = `
+		delete from rooms
+			where id = $1 and user_uid = $2;
+	`
+)
